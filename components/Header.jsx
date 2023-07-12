82009cc7916ea5410c184847e16db44f3885e207
codeoz <codeoz@proton.me>
2023-07-13 00:35:52 +0300

    create header-MenuItem

---
+++ components/Header.jsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import MenuItem from "./MenuItem";
+import { AiFillHome } from "react-icons/ai";
+import { BsFillInfoCircleFill } from "react-icons/bs";
+
+export default function Header() {
+  return (
+    <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
+      <div className="flex">
+        <MenuItem title="HOME" adress="/" Icon={AiFillHome} />
+        <MenuItem title="ABOUT" adress="/about" Icon={BsFillInfoCircleFill} />
+      </div>
+      <div className="flex items-center space-x-5">
+        <Link href='/'>
+          <h2 className="text-2xl">
+            <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">
+              IMDb
+            </span>
+            <span className="text-xl hidden sm:inline">
+              Clone
+            </span>
+          </h2>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+
+// MenuItem.jsx => Header.jsx'in iceriklerini olusturmak icin kullanilan componenttir. Propslarla bilgileri gonderdik
---
+++ components/MenuItem.jsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function MenuItem({ title, adress, Icon }) {
+    return (
+        <div>
+            <Link href={adress} className="mx-4 lg:mx-6 hover:text-amber-600">
+                {/* iconlar sadece sm altinda gorunecek */}
+                <Icon className='text-2xl sm:hidden mx-4' />
+                <p className="hidden sm:inline my-2 text-sm">
+                    {title}
+                </p>
+            </Link>
+        </div>
+    )
+}
+
+// MenuItem.jsx => Header.jsx'in iceriklerini olusturmak icin kullanilan componenttir

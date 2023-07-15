822b4e978cd9353a67cb1759d4439a288d5d424b
codeoz <codeoz@proton.me>
2023-07-15 16:30:59 +0300

    add card componenet and scrollbar remove

---
+++ components/Card.jsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FiThumbsUp } from "react-icons/fi";
+
+export default function Card({ result }) {
+    return (
+        <div className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-300 group">
+            <Link href={`/movie/${result.id}`}>
+                <Image src={`https://image.tmdb.org/t/p/original/${result.backdrop_path || result.poster_path}`}
+                    alt="image"
+                    width={500}
+                    height={300}
+                    className="sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-300"
+                    placeholder="blur"
+                    blurDataURL='/spinner.svg'
+                    // mobil cihazlarda resimlerin boyutu ayarlanıyor
+                    style={{ width: '100%', height: 'auto' }}
+                />
+                <div className="py-2">
+                    <p className="line-clamp-2 text-base">{result.overview}</p>
+                    <h2 className="truncate text-lg font-bold">
+                        {result.title || result.name}
+                    </h2>
+                    <p className="flex items-center">
+                        {result.release_date || result.first_air_date}
+                        <FiThumbsUp className="mr-1 ml-3 h-5" />
+                        {result.vote_count}
+                    </p>
+                </div>
+            </Link>
+        </div>
+    )
+}
+
+//small one column, medium two column, large three column, extra large four column, Tv five column
+//Bu islemler icin hic route olusturmadik. Sadece parametre olarak gelen genre'i kullandik
+//Results.jsx den props aliyor
+//blurDataURL ile resim yüklenene kadar spinner görüntüsü veriyor
+//truncate paragrafları tek satırda gösterir
+//https://tailwindcss.com/docs/line-clamp
--- components/Results.jsx
+++ components/Results.jsx
@@ -1,14 +1,18 @@
+import Card from "./Card";
+
 export default function Results({ results }) {
     return (
-        <div>
+        //Kardlarin çözünürlüğe göre grid yapısı burada ayarlandı
+        <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-full sm:mx-5 py-4">
             {
                 results.map((result) => (
-                    <div key={result.id}>
-                        {result.original_title}
-                    </div>
+                    <Card key={result.id} result={result} />
                 ))
 
             }
         </div>
     )
 }
+
+//page.js dosyasından props alıyor. Alınan verileri map ile dönüyor bu sayede 20 tane filmi ekrana basıyor
+//Card.js'ye props olarak result'u gönderiyor
